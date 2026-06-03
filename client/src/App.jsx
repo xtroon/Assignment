ac327b46f50cdfe3ba7725364a809539769465e0
Ome Tiwari <omt81144@gmail.com>
2026-06-04 00:40:08 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,21 @@ import Login from './pages/Login'
 import Home from './pages/Home'
 import Product from './pages/Product'
 
+const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+  return children;
+};
+
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/home" replace />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/products" element={<Product />} />
+        <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+        <Route path="/products" element={<ProtectedRoute><Product /></ProtectedRoute>} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
