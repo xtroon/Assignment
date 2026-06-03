8fda797388cb68363c8a9283e6ce3c7e715ef862
Ome Tiwari <omt81144@gmail.com>
2026-06-04 00:48:59 +0530

    Revert back to BrowserRouter and add vercel.json configuration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,65 @@
-import React from 'react'
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Login from './pages/Login'
-import Home from './pages/Home'
-import Product from './pages/Product'
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
 
 const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem("token");
+
   if (!token) {
     return <Navigate to="/login" replace />;
   }
+
   return children;
 };
 
 const App = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/home" replace />} />
-        <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-        <Route path="/products" element={<ProtectedRoute><Product /></ProtectedRoute>} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </HashRouter>
-  )
-}
+        {/* Default Route */}
+        <Route
+          path="/"
+          element={<Navigate to="/login" replace />}
+        />
 
-export default App
+        {/* Public Route */}
+        <Route path="/login" element={<Login />} />
+
+        {/* Protected Routes */}
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/products"
+          element={
+            <ProtectedRoute>
+              <Product />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Invalid URL */}
+        <Route
+          path="*"
+          element={<Navigate to="/login" replace />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
